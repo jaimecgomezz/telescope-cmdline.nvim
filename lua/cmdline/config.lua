77a60eae619c49d968fc518bdd37871c5b2733e5
jaimecgomezz <jaimecgomezwork@gmail.com>
2024-09-03 21:51:03 -0600

    refactor: Improve existing mappings and add new ones

--- lua/cmdline/config.lua
+++ lua/cmdline/config.lua
@@ -1,58 +1,60 @@
 local defaults = {
-  highlights  = {
-    icon = "Include",
-  },
-  icons       = {
-    history = " ",
-    command = " ",
-    number  = "󰴍 ",
-    system  = "",
-    unknown = "",
-  },
-  picker      = {
-    layout_config = {
-      width  = 70,
-      height = 15,
-    }
-  },
-  completions = {
-    'command',
-  },
-  mappings    = {
-    complete      = '<Tab>',
-    run_selection = '<C-CR>',
-    run_input     = '<CR>',
-  },
-  overseer    = {
-    enabled = true,
-  },
-  output_pane = {
-    enabled = false,
-    min_lines = 5,
-    max_height = 25,
-  }
+	highlights = {
+		icon = "Include",
+	},
+	icons = {
+		history = " ",
+		command = " ",
+		number = "󰴍 ",
+		system = "",
+		unknown = "",
+	},
+	picker = {
+		layout_config = {
+			width = 70,
+			height = 15,
+		},
+	},
+	completions = {
+		"command",
+	},
+	mappings = {
+		next = "<Tab>",
+		previous = "<S-Tab>",
+		complete = "<C-Space>",
+		run_selection = "<C-CR>",
+		run_input = "<CR>",
+	},
+	overseer = {
+		enabled = true,
+	},
+	output_pane = {
+		enabled = false,
+		min_lines = 5,
+		max_height = 25,
+	},
 }
 
 local config = {
-  namespace_id = vim.api.nvim_create_namespace('cmdline'),
-  values = {
-    mappings = {}
-  }
+	namespace_id = vim.api.nvim_create_namespace("cmdline"),
+	values = {
+		mappings = {},
+	},
 }
 
 -- @param user_defaults table: user options
 function config.set_defaults(user_defaults)
-  user_defaults = vim.F.if_nil(user_defaults, {})
-  config.values = vim.tbl_deep_extend("keep", user_defaults, defaults)
+	user_defaults = vim.F.if_nil(user_defaults, {})
+	config.values = vim.tbl_deep_extend("keep", user_defaults, defaults)
 end
 
 -- Retreives custom configuration or default setings
 function config.get()
-  if config.values == nil or config.values == {} then
-    return config.defaults
-  else
-    return config.values
-  end
+	if config.values == nil or config.values == {} then
+		return config.defaults
+	else
+		return config.values
+	end
 end
 
 return config
--- lua/telescope/_extensions/cmdline.lua
+++ lua/telescope/_extensions/cmdline.lua
@@ -1,92 +1,94 @@
 local has_telescope, telescope = pcall(require, "telescope")
 
 if not has_telescope then
-  error("This plugins requires nvim-telescope/telescope.nvim")
+	error("This plugins requires nvim-telescope/telescope.nvim")
 end
 
-local cmdline = require('cmdline')
-local config = require('cmdline.config')
-local action = require('cmdline.actions')
+local cmdline = require("cmdline")
+local config = require("cmdline.config")
+local action = require("cmdline.actions")
+local taction = require("telescope.actions")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local entry_display = require("telescope.pickers.entry_display")
 
-local sorter = require('cmdline.sorter')
+local sorter = require("cmdline.sorter")
 
 local displayer = entry_display.create({
-  separator = " ",
-  items = {
-    { width = 2 },
-    { remaining = true },
-  },
+	separator = " ",
+	items = {
+		{ width = 2 },
+		{ remaining = true },
+	},
 })
 
 local make_display = function(entry)
-  local config = assert(config.get(), "No config found")
-  return displayer({
-    { entry.icon, config.highlights.icon },
-    { entry.cmd },
-  })
+	local config = assert(config.get(), "No config found")
+	return displayer({
+		{ entry.icon, config.highlights.icon },
+		{ entry.cmd },
+	})
 end
 
 local make_finder = function(config)
-  return finders.new_dynamic({
-    fn = cmdline.autocomplete,
-    entry_maker = function(entry)
-      entry.icon = config.icons[entry.type]
-      entry.id = entry.index
-      entry.value = entry.cmd
-      entry.ordinal = entry.cmd
-      entry.display = make_display
-      return entry
-    end,
-  })
+	return finders.new_dynamic({
+		fn = cmdline.autocomplete,
+		entry_maker = function(entry)
+			entry.icon = config.icons[entry.type]
+			entry.id = entry.index
+			entry.value = entry.cmd
+			entry.ordinal = entry.cmd
+			entry.display = make_display
+			return entry
+		end,
+	})
 end
 
 local make_picker = function(opts)
-  local config = assert(config.get(), "No config found")
-  return pickers.new(config.picker, {
-    prompt_title = "Cmdline",
-    prompt_prefix = " : ",
-    finder = make_finder(config),
-    sorter = sorter(opts),
-    attach_mappings = function(_, map)
-      map("i", config.mappings.complete, action.complete_input)     -- <Tab>
-      map("i", config.mappings.run_input, action.run_input)         -- <CR>
-      map("i", config.mappings.run_selection, action.run_selection) -- <C-CR>
-      map("i", "<C-e>", action.edit)
-      require("telescope.actions").close:enhance {
-        post = function()
-          cmdline.preview.clean(vim.api.nvim_win_get_buf(0))
-        end,
-      }
-      return true
-    end,
-  })
+	local config = assert(config.get(), "No config found")
+	return pickers.new(config.picker, {
+		prompt_title = "Cmdline",
+		prompt_prefix = " : ",
+		finder = make_finder(config),
+		sorter = sorter(opts),
+		attach_mappings = function(_, map)
+			map("i", config.mappings.next, taction.move_selection_next) -- <Tab>
+			map("i", config.mappings.previous, taction.move_selection_previous) -- <S-Tab>
+			map("i", config.mappings.complete, action.complete_input) -- <C-Space>
+			map("i", config.mappings.run_input, action.run_input) -- <CR>
+			map("i", config.mappings.run_selection, action.run_selection) -- <C-CR>
+			require("telescope.actions").close:enhance({
+				post = function()
+					cmdline.preview.clean(vim.api.nvim_win_get_buf(0))
+				end,
+			})
+			return true
+		end,
+	})
 end
 
 local telescope_cmdline = function(opts)
-  cmdline.preload()
-  local picker = make_picker(opts)
-  picker:find()
+	cmdline.preload()
+	local picker = make_picker(opts)
+	picker:find()
 end
 
 local cmdline_visual = function(opts)
-  cmdline.preload()
-  local picker = make_picker(opts)
-  picker:find()
-  picker:set_prompt("'<,'> ")
+	cmdline.preload()
+	local picker = make_picker(opts)
+	picker:find()
+	picker:set_prompt("'<,'> ")
 end
 
 return telescope.register_extension({
-  setup   = function(ext_config, user_config)
-    if vim.fn.has('nvim-0.10') == 0 then
-      vim.notify('Cmdline extension requires Neovim 0.10 or higher', vim.log.levels.ERROR, {})
-    end
-    require("cmdline.config").set_defaults(ext_config)
-  end,
-  exports = {
-    cmdline = telescope_cmdline,
-    visual  = cmdline_visual,
-  }
+	setup = function(ext_config, user_config)
+		if vim.fn.has("nvim-0.10") == 0 then
+			vim.notify("Cmdline extension requires Neovim 0.10 or higher", vim.log.levels.ERROR, {})
+		end
+		require("cmdline.config").set_defaults(ext_config)
+	end,
+	exports = {
+		cmdline = telescope_cmdline,
+		visual = cmdline_visual,
+	},
 })
